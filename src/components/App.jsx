28d8275db44b9158f4196c663b94a334a6659356
Danil <i@cadernews.ru>
2022-08-17 09:34:45 +0300

    fix commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,44 +5,44 @@ import TaskList from './TaskList'
 import Footer from './Footer'
 
 function App() {
-	const [inputValue, setInputValue] = useState('')
-	const [todos, setTodos] = useState([])
-	const [status, setStatus] = useState('all')
-	const [filterTodo, setFilterTodo] = useState([])
+  const [inputValue, setInputValue] = useState('')
+  const [todos, setTodos] = useState([])
+  const [status, setStatus] = useState('all')
+  const [filterTodo, setFilterTodo] = useState([])
 
-	useEffect(() => {
-		filterHandler()
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [todos, status])
+  useEffect(() => {
+    filterHandler()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [todos, status])
 
-	const filterHandler = () => {
-		switch (status) {
-			case 'completed':
-				setFilterTodo(todos.filter(t => t.completed))
-				break
-			case 'active':
-				setFilterTodo(todos.filter(t => !t.completed))
-				break
-			default:
-				setFilterTodo(todos)
-				break
-		}
-	}
+  const filterHandler = () => {
+    switch (status) {
+      case 'completed':
+        setFilterTodo(todos.filter((t) => t.completed))
+        break
+      case 'active':
+        setFilterTodo(todos.filter((t) => !t.completed))
+        break
+      default:
+        setFilterTodo(todos)
+        break
+    }
+  }
 
-	return (
-		<section className='todoapp'>
-			<NewTaskForm
-				inputValue={inputValue}
-				setInputValue={setInputValue}
-				todos={todos}
-				setTodos={setTodos}
-			/>
-			<section className='main'>
-				<TaskList todos={todos} setTodos={setTodos} filterTodo={filterTodo} />
-				<Footer todos={todos} setTodos={setTodos} setStatus={setStatus} />
-			</section>
-		</section>
-	)
+  return (
+    <section className="todoapp">
+      <NewTaskForm
+        inputValue={inputValue}
+        setInputValue={setInputValue}
+        todos={todos}
+        setTodos={setTodos}
+      />
+      <section className="main">
+        <TaskList todos={todos} setTodos={setTodos} filterTodo={filterTodo} />
+        <Footer todos={todos} setTodos={setTodos} setStatus={setStatus} />
+      </section>
+    </section>
+  )
 }
 
 export default App
